af265dfd342085d10e2da89a4a36246fe9d88f7d
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-12 23:40:09 -0300

    Updates

---
+++ precache-manifest.f83a03fc494909853d932e0c6c755f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1119044aa56118f8be92b61f78e375c",
+    "url": "/%20//rodrigoesquel.github.io/react-clase-dos/index.html"
+  },
+  {
+    "revision": "494a8c91d22217e790b4",
+    "url": "/%20//rodrigoesquel.github.io/react-clase-dos/static/css/main.46cfb4fc.chunk.css"
+  },
+  {
+    "revision": "b46cb2da92e4c71f116d",
+    "url": "/%20//rodrigoesquel.github.io/react-clase-dos/static/js/2.4ecacb68.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/%20//rodrigoesquel.github.io/react-clase-dos/static/js/2.4ecacb68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "494a8c91d22217e790b4",
+    "url": "/%20//rodrigoesquel.github.io/react-clase-dos/static/js/main.b37d1651.chunk.js"
+  },
+  {
+    "revision": "ef0fccf254f7fc610ae5",
+    "url": "/%20//rodrigoesquel.github.io/react-clase-dos/static/js/runtime-main.06db5658.js"
+  }
+]);
